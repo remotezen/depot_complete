09264939b9864d5b2e7a1b2f01a61138b4e49972
remotezen <remotezen@bell.net>
2016-06-02 11:13:13 -0400

    sorting out mail test

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,23 @@
+class OrderNotifier < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Store order Confirmation' 
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped(order)
+    @order = order
+
+    mail to: order.email, subject: 'store order shipped' 
+  end
+end
